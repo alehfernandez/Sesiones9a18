98f7bd19040afaa1626ad5582bedc0c22bab8b5d
alehfernandez <alehfernandez@gmail.com>
2023-05-03 20:16:54 -0300

    Ejercicios 9 a 18

---
+++ Patrones9a18/src/Sesion3DataBase/DataBaseConnection.java
@@ -0,0 +1,7 @@
+package Sesion3DataBase;
+
+public interface DataBaseConnection {
+    void connect();
+    void disconnect();
+    // Otros métodos necesarios para interactuar con la base de datos
+}
